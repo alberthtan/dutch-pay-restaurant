233dc79e68429d0a98c582ccdaaf142ae7591abf
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-04-06 13:08:33 -0400

    allow index.html to handle all routes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', '../public/index.html'));
 });
 
