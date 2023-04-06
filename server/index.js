f5d157bedb4bfc9bbfe1faeca6e5000e708dab49
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-04-06 13:02:21 -0400

    allow index.html to handle all routes

--- server/index.js
+++ server/index.js
@@ -7,20 +7,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', '../public/index.html'));
 });
 
 app.get('/apple-app-site-association', function(req, res) {
     res.sendFile(path.join(__dirname, '/apple-app-site-association'));
-});
-
-app.get('/terms-of-use', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'terms-of-use.html'));
-});
-
-app.get('/privacy-policy', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'privacy-policy.html'));
 });
 
 const port = process.env.PORT || 5000;
